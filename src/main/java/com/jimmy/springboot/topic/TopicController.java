6b36c7a4b0295e97ab4e7f34e3293b107e173123
Jimmy Tang <blay.tenshi@gmail.com>
2019-05-15 22:39:21 +1000

    Request Mappings for full rest!

--- src/main/java/com/jimmy/springboot/topic/TopicController.java
+++ src/main/java/com/jimmy/springboot/topic/TopicController.java
@@ -1,20 +1,42 @@
 package com.jimmy.springboot.topic;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class TopicController {
 
+    @Autowired
+    private TopicService topicService;
+
     @RequestMapping("/topics")
     public List<Topic> getAllTopics() {
-        return Arrays.asList(
-                new Topic("1", "Spring Framework", "Spring Framework Description"),
-                new Topic("2", "Spring Boot", "Spring Boot Description"),
-                new Topic("3", "Spring WebMVC", "Spring WebMVC Description")
-        );
+        return topicService.getAllTopics();
+    }
+
+    @RequestMapping("/topics/{topicId}")
+    // could have mapped it to {id} to match the parameter being passed into getTopic
+    // but did this to show we can map what we wanna map
+    public Topic getTopic(@PathVariable("topicId") String id) {
+        return topicService.getTopic(id);
+    }
+
+    // if you wanna be verbose you can set the method=Request.GET for default @RequestMappings
+    @RequestMapping(method = RequestMethod.POST, value = "/topics")
+    // @RequestBody takes a JSON object in the request body and map it to the topic object
+    public void addTopic(@RequestBody Topic topic) {
+        topicService.addTopic(topic);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+        topicService.updateTopic(id, topic);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+    public void deleteTopic(@PathVariable String id) {
+        topicService.deleteTopic(id);
     }
 }
---
+++ src/main/java/com/jimmy/springboot/topic/TopicService.java
@@ -0,0 +1,44 @@
+package com.jimmy.springboot.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
+            new Topic("1", "Spring Framework", "Spring Framework Description"),
+            new Topic("2", "Spring Boot", "Spring Boot Description"),
+            new Topic("3", "Spring WebMVC", "Spring WebMVC Description")
+    ));
+
+    public List<Topic> getAllTopics() {
+        return topics;
+    }
+
+    public Topic getTopic(String id) {
+        // Hmmm what is .get()
+        return topics.stream().filter(topic -> topic.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(String id, Topic topic) {
+        for (int i = 0; i < topics.size(); i++) {
+            Topic t = topics.get(i);
+            if (t.getId().equals(id)) {
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id) {
+        topics.removeIf(t -> t.getId().equals(id));
+    }
+}
